2e8940fec4696380951a2d9d72a65bea054f86ef
jtlovesbaseball <JTT@duck.com>
2023-01-14 16:03:40 -0500

    added first course.

--- src/Course.py
+++ src/Course.py
@@ -1,3 +1,6 @@
+import os.path as osp
+import pickle as pkl
+
 class Course(object):
     """
     A "Course" here is defined as any number of holes that you play.
@@ -7,6 +10,7 @@ class Course(object):
 
     def __init__(self, course_name, holes=None):
         self.name = course_name
+        self.savename = course_name.replace(' ', '_') + '.pkl'
         self.course_len = len(holes)
         self.holes = [] if holes is None else holes
 
@@ -19,6 +23,14 @@ class Course(object):
             self.total_yds += yds
             self.total_par += par
 
+    def save_path(self):
+        file_loc = osp.abspath(__file__)
+        save_path = osp.join('data', 'courses', self.savename)
+        return save_path
+
     def __str__(self):
-        retstr = """"""
+        retstr = """Course name: %s
+Length: %d holes
+Par: %d strokes
+Distance: %d Yards""" % (self.name, self.course_len, self.total_par, self.total_yds)
         return retstr
